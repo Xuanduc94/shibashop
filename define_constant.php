f4e4a6c1e721b3dc1852ecbd6b5f65df27fb51ba
Xuan Duc <xuanductq1994@gmail.com>
2024-03-14 08:57:27 +0700

    change the config

--- define_constant.php
+++ define_constant.php
@@ -2,11 +2,11 @@
 
 /************ Database ************/
 
-define('CMS_DB_HOST', 'localhost');
-define('CMS_DB_USER', 'root');
-define('CMS_DB_PASSWORD', '');
-define('CMS_DB_NAME', 'pos_new_7');
-define('CMS_DB_PREFIX', 'cms_');
+// define('CMS_DB_HOST', 'localhost');
+// define('CMS_DB_USER', 'root');
+// define('CMS_DB_PASSWORD', '');
+// define('CMS_DB_NAME', 'pos_new_7');
+// define('CMS_DB_PREFIX', 'cms_');
 
 /************ ORTHER ************/
 define('CMS_BASE_URL', '');
